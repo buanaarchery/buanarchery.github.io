742ba4df51098119eabbb1123af51cdbb0662edd
Arti Hikmatullah Perbawana Sakti Buana <sakti@buana.studio>
2021-12-12 11:42:14 +0700

    remodel into flask

--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  get '/' => 'pages#home'
-  get '/about' => 'pages#about'
-  get '/contact' => 'pages#contact'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
