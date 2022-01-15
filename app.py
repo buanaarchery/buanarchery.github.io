ec2a5307d03e95beea8fed883c76059c512baac2
Arti Hikmatullah Perbawana Sakti Buana <sakti@buana.studio>
2022-01-15 20:20:32 +0700

    refined

--- app.py
+++ app.py
@@ -11,8 +11,11 @@ app = Flask(__name__,
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 
 
 
-if __name__ == '__main__': app.run(debug=True)
+if __name__ == '__main__': app.run(debug=True, threaded=True, port=80)
